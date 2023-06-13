20e0894a86e3b2b85781771a959ab8b2ab8ca1da
Oleg Brusnik <oleg.brusnik.93@mail.ru>
2023-06-13 22:07:31 +0500

    Add index.html and script.js, images of completed homework

---
+++ script.js
@@ -0,0 +1,51 @@
+// Задание 1
+
+// let userName = prompt("Введите ваше имя");
+
+// alert("Здравствуйте, " + userName);
+
+//  Задание 2
+
+// let number = +prompt("Введите желаемое число");
+
+// let power = +prompt("Введите желаемую степень");
+
+// alert(number ** power);
+
+// Задание 3
+
+// let textToFilter = prompt("Введите текст");
+
+// if (textToFilter != "some text") {
+//   textToFilter = "some text";
+// } else {
+//   textToFilter = "another text";
+// }
+
+// alert(textToFilter);
+
+// Задание 4 - файлы приложены
+
+// Задание 5
+
+// let randomNum = +prompt("Введите число");
+
+// if (randomNum < 0) {
+//   alert("less than zero");
+// } else if (randomNum === 0) {
+//   alert(1);
+// } else alert((randomNum *= 10));
+
+// Задание 6
+
+let userNumber = +prompt("Введите число");
+
+let result;
+
+if (userNumber > 5) {
+  result = 1;
+} else {
+  result = 0;
+}
+
+console.log(result);
