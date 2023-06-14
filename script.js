a28950ae42617796d14dc1a16cd6d5118809c5c2
Oleg Brusnik <oleg.brusnik.93@mail.ru>
2023-06-15 02:21:07 +0500

    homework done

--- script.js
+++ script.js
@@ -38,14 +38,88 @@
 
 // Задание 6
 
-let userNumber = +prompt("Введите число");
+// let userNumber = +prompt("Введите число");
 
-let result;
+// let result;
 
-if (userNumber > 5) {
-  result = 1;
+// if (userNumber > 5) {
+//   result = 1;
+// } else {
+//   result = 0;
+// }
+
+// console.log(result);
+
+// Задание 7
+
+// let firstNumber = prompt("Введите первое число");
+
+// let secondNumber = prompt("Введите второе число");
+
+// if (firstNumber > secondNumber) {
+//   console.log("Большее число -", firstNumber);
+// } else if (firstNumber < secondNumber) {
+//   console.log("Большее число -", secondNumber);
+// } else {
+//   console.log("Числа одинаковы");
+// }
+
+// Задание 8
+
+// let number1 = prompt("Введите первое число");
+
+// let number2 = prompt("Введите второе число");
+
+// if (number1 % number2 === 0) {
+//   console.log("Первое число кратно второму");
+// } else {
+//   console.log("Первое число не кратно второму");
+// }
+
+// Задание 9
+
+// let avgScore = prompt("Введите ваш средний балл");
+
+// if (avgScore <= 4) {
+//   console.log("Неудовлетворительно");
+// } else if (avgScore <= 8) {
+//   console.log("Удовлетворительно");
+// } else {
+//   console.log("Отлично");
+// }
+
+// Задание 10
+
+// let examScore = prompt("Введите балл за экзамен");
+
+// let projectQuantity = prompt("Введите количество выполненных проектов");
+
+// let total;
+
+// if ((projectQuantity >= 10 && examScore >= 90)) {
+//   total = 100;
+// } else if ((projectQuantity >= 5 && examScore >= 75)) {
+//   total = 90;
+// } else if ((projectQuantity >= 2 && examScore >= 50)) {
+//   total = 75;
+// } else {
+//   total = 0;
+// }
+
+// console.log(total);
+
+// Задание 11
+
+let rentDays = prompt("Количество дней аренды");
+
+let discount;
+
+if (rentDays >= 7) {
+  discount = 50;
+} else if (rentDays >= 3) {
+  discount = 20;
 } else {
-  result = 0;
+  discount = 0;
 }
 
-console.log(result);
+console.log("Итоговая стоимость аренды - ", rentDays * 40 - discount);
